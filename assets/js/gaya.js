00b548c6dd936dd6cc537c861a5ba07decc00ce3
Etem Hyusnev <mr.cyberpower@gmail.com>
2016-09-24 10:33:45 +0300

    Removed disqus js func, embeddegd disqus code, added paginate gem

--- assets/js/gaya.js
+++ assets/js/gaya.js
@@ -104,31 +104,8 @@
 
 /*
  * Show disqus comments
+   DELETED!	
  */
-jQuery(document).ready(function() {
-
-  jQuery(".post").fitVids();
-
-  // Load discus comment
-  function initDisqusComments(){
-
-    if(config.disqus_shortname != '' && config.disqus_shortname != null && config.disqus_shortname != undefined) {
-      var disqus_shortname = config.disqus_shortname;
-      (function() {
-      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-      })();
-    }else {
-      alert("Please check Disqus short name configuration on your _config.yml");
-    }
-  }
-
-  //$('.load-view').click(function(){
-    initDisqusComments();
-    //$(this).fadeOut(200);
-  //});
-});
 
 /*
  * Scroll to top button
